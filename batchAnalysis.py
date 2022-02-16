0528f099ff65c44e13e8363e17d0dc0d506e8471
Aditya Asopa <aditya.ncbs@gmail.com>
2022-02-16 19:54:26 +0530

    added flag for verbose output

--- batchAnalysis.py
+++ batchAnalysis.py
@@ -3,11 +3,12 @@ import sys
 import pathlib
 import argparse
 import importlib
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from os import devnull
 
 import analysis
 from eidynamics.plot_maker  import dataframe_to_plots
 from eidynamics             import ephys_classes
-# from all_cells              import *
 
 
 def batch_analysis(cellDirectory, add_cell_to_database=False, export_training_set=False, save_plots=False):
@@ -42,6 +43,41 @@ def meta_analysis(cellFile):
 def meta_plot(allCellsFile):
     pass
 
+def main(args):    
+    all_cells_filename = pathlib.Path(args.file).stem
+    all_cells_filepath = pathlib.Path(args.file)
+    cell_list = importlib.import_module(all_cells_filename, all_cells_filepath)
+    if args.analyse:
+        all_cells = cell_list.all_cells
+        project_path_root = cell_list.project_path_root
+        print("Analysing all catalogued cells recordings...")
+        for cellDirectory in all_cells:
+                savedCellFile = batch_analysis((project_path_root / cellDirectory),add_cell_to_database=True, export_training_set=True, save_plots=True)
+                print("Data saved in cell file: ",savedCellFile)
+
+
+    elif args.test:
+        test_cells = cell_list.test_cells
+        print("Checking if analysis pipeline is working...")
+        for cellDirectory in test_cells:
+                savedCellFile = batch_analysis(cellDirectory,add_cell_to_database=False, export_training_set=True, save_plots=True)
+                print(savedCellFile)
+        print('All Tests Passed!')
+
+    else:
+        all_cells = cell_list.all_cells
+        project_path_root = cell_list.project_path_root
+        for cellDirectory in all_cells:
+            try:
+                print("Looking for analysed cell pickles to plot directly from...")
+                cf = [os.path.join(cellDirectory, pickleFile) for pickleFile in os.listdir(cellDirectory) if pickleFile.endswith("cell.pkl")]
+                print("Plotting from: ",cf[0])
+                batch_plot(cf[0])
+            except FileNotFoundError:
+                print("Cell pickle not found. Beginning analysis.")
+                savedCellFile = batch_analysis((project_path_root / cellDirectory),add_cell_to_database=True, export_training_set=True, save_plots=True)
+                print("Data saved in cell file: ",savedCellFile)
+                batch_plot(savedCellFile)
 
 """Argument Parser"""
 
@@ -49,49 +85,30 @@ parser = argparse.ArgumentParser(description="Run the main analysis program on a
 
 parser.add_argument("file",   help="Required, python file that has list of selected cells to run batch analysis")
 
-group1  = parser.add_mutually_exclusive_group()
-group1.add_argument( "-v", "--verbose",action="store_true")
-group1.add_argument( "-q", "--quiet",  action="store_true")
+parser.add_argument( "-v", "--verbose",action="store_true")
 
-group2  = parser.add_mutually_exclusive_group()
-group2.add_argument("-t", "--test",    action="store_true", help="Flag to run a code test")
-group2.add_argument("-a", "--analyse", action="store_true", help="Flas to run batch analysis")
+group  = parser.add_mutually_exclusive_group()
+group.add_argument("-t", "--test",    action="store_true", help="Flag to run a code test")
+group.add_argument("-a", "--analyse", action="store_true", help="Flas to run batch analysis")
+
 parser.add_argument("-p", "--plot",    action="store_true", help="to display plots")
 
 args = parser.parse_args()
-all_cells_filename = pathlib.Path(args.file).stem
-all_cells_filepath = pathlib.Path(args.file)
-cell_list = importlib.import_module(all_cells_filename, all_cells_filepath)
 
+# to suppress the print statements, if verbose flag == False                
+@contextmanager 
+def suppress_stdout_stderr():
+    """A context manager that redirects stdout and stderr to devnull"""
+    with open(devnull, 'w') as fnull:
+        with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+            yield (err, out)
 
-if args.analyse:
-    all_cells = cell_list.all_cells
-    project_path_root = cell_list.project_path_root
-    print("Analysing all catalogued cells recordings...")
-    for cellDirectory in all_cells:
-            savedCellFile = batch_analysis((project_path_root / cellDirectory),add_cell_to_database=True, export_training_set=True, save_plots=True)
-            print("Data saved in cell file: ",savedCellFile)
-
-
-elif args.test:
-    test_cells = cell_list.test_cells
-    print("Checking if analysis pipeline is working...")
-    for cellDirectory in test_cells:
-            savedCellFile = batch_analysis(cellDirectory,add_cell_to_database=False, export_training_set=True, save_plots=True)
-            print(savedCellFile)
-    print('All Tests Passed!')
-
+if args.verbose:
+    main(args)
 else:
-    all_cells = cell_list.all_cells
-    project_path_root = cell_list.project_path_root
-    for cellDirectory in all_cells:
-        try:
-            print("Looking for analysed cell pickles to plot directly from...")
-            cf = [os.path.join(cellDirectory, pickleFile) for pickleFile in os.listdir(cellDirectory) if pickleFile.endswith("cell.pkl")]
-            print("Plotting from: ",cf[0])
-            batch_plot(cf[0])
-        except FileNotFoundError:
-            print("Cell pickle not found. Beginning analysis.")
-            savedCellFile = batch_analysis((project_path_root / cellDirectory),add_cell_to_database=True, export_training_set=True, save_plots=True)
-            print("Data saved in cell file: ",savedCellFile)
-            batch_plot(savedCellFile)
+    with suppress_stdout_stderr():
+        main(args)
+
+print('All Done!')
+
+
